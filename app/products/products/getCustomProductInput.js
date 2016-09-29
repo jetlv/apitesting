56fe835b0a70fd695e90bbcce6ee1cae03bc781a
jet <jetlyu521@yahoo.com>
2016-09-29 16:05:54 +0800

    #1 add a template #2 intendly make an error to chekc if jenkins can output specific information

---
+++ app/products/products/getCustomProductInput.js
@@ -0,0 +1,40 @@
+/// <reference path="../../../include.d.ts" />
+var conf = require('../../../configuration.js');
+var CONST = conf.CONST;
+var R = require('../../../req.js');
+var expect = R.expect;
+var __path = R.__path;
+var Promise = require('bluebird');
+var supertest = R.supertest;
+
+
+var env = CONST.APP_ADDRESS_TESTENV;
+var tester = supertest.agent(env);
+
+
+describe('高定-选项获取', function () {
+
+    it('基础验证-中文', function (done) {
+        var url = __path(__filename);
+        var params = 'languageId=1';
+        tester.get(url + params)
+            .end(function (err, res) {
+                new Promise(function (resolve, reject) {
+                    var result = res.body.result;
+                    expect(result.length).equal(8);
+                    resolve(res.body);
+                }).then(body => {
+                    expect(body.msg).equal("操作成功");
+                    return body;
+                }).then(body => {
+                    expect(body.code).equal("0");
+                    done();
+                    return body;
+                }).catch(err => {
+                    console.log(env + url + params);
+                    console.log(JSON.stringify(res.body));
+                    done(err);
+                });
+            });
+    });
+});
--- configuration.js
+++ configuration.js
@@ -1,7 +1,8 @@
 /// <reference path="./include.d.ts" />
 
 module.exports.CONST = {
-    APP_ADDRESS : "http://app.milanoo.com",
-    SEP_win32 : "\\"   // Windowsd系统分隔符 
+    APP_ADDRESS: "http://app.milanoo.com",
+    APP_ADDRESS_TESTENV: 'http://192.168.11.67:8080', //测试站域名
+    SEP_win32: "\\"   // Windowsd系统分隔符 
 };
 
